1bbe5f2c3ecbe98fe5a6011a5c11339b85820273
minbaby01 <ongbeo111@gmail.com>
2025-10-21 21:10:56 +0700

    update security

--- api/index.ts
+++ api/index.ts
@@ -2,29 +2,35 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { bot } from "../src/index";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    try {
-        // is admin
-        const adminId = process.env.ADMIN_ID;
-        if (req.body?.message?.from?.id != adminId) {
-            return res.status(401).json({
-                message: 'Who are you'
-            });
-        }
+  try {
+    const adminId = process.env.ADMIN_ID;
+    const secret = process.env.TG_SECRET_TOKEN;
+    const headerToken = req.headers["x-telegram-bot-api-secret-token"];
 
-        if (req.method === 'POST') {
-            await bot.handleUpdate(req.body);
-            return res.status(200).json({
-                message: 'OK'
-            });
-        }
-
-        res.status(200).json({
-            message: 'Hello'
-        });
-    } catch (err) {
-        console.error('Handler error:', err);
-        res.status(500).json({
-            error: "Error"
-        });
+    if (headerToken !== secret) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
+
+    if (req.body.message.from.id != adminId) {
+      return res.status(200).json({
+        message: "Hello",
+      });
+    }
+
+    if (req.method === "POST") {
+      await bot.handleUpdate(req.body);
+      return res.status(200).json({
+        message: "OK",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Hello guest",
+    });
+  } catch (err) {
+    console.error("Handler error:", err);
+    res.status(500).json({
+      error: "Error",
+    });
+  }
 }
