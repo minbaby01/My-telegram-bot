4f4e0c43bb5ba118946038d2033a4e3d76ed7b20
minbaby01 <ongbeo111@gmail.com>
2025-12-09 18:42:41 +0700

    up

--- src/services/moneyService.ts
+++ src/services/moneyService.ts
@@ -3,11 +3,10 @@ import { GetMastercardRateResponse, GetVisaRateResponse } from "../types/money";
 
 const fromCurr = "VND";
 const toCurr = "USD";
+const amount = 1;
+const fee = 0;
 
 export const getVisaUsdRateService = async (date: string) => {
-  const amount = 1;
-  const fee = 0;
-
   const { data } = await axios.get<GetVisaRateResponse>(
     `https://www.visa.com.vn/cmsapi/fx/rates?amount=${amount}&fee=${fee}&utcConvertedDate=${encodeURIComponent(
       date
@@ -15,15 +14,19 @@ export const getVisaUsdRateService = async (date: string) => {
       date
     )}&fromCurr=${fromCurr}&toCurr=${toCurr}`
   );
+
+  if (!data) {
+    throw "Visa error";
+  }
   return data;
 };
 
 export const getMastercardUsdRateService = async (date: string) => {
-  const amount = 1;
-  const fee = 0;
-
   const { data } = await axios.get<GetMastercardRateResponse>(
     `https://www.mastercard.com/marketingservices/public/mccom-services/currency-conversions/conversion-rates?exchange_date=${date}&transaction_currency=${toCurr}&cardholder_billing_currency=${fromCurr}&bank_fee=${fee}&transaction_amount=${amount}`
   );
+  if (!data) {
+    throw "Mastercard error";
+  }
   return data.data;
 };
