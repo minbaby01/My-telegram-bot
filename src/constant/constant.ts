ccff1685f917eb949b9530655d4ba8f9dff02b20
minbaby01 <ongbeo111@gmail.com>
2026-02-18 23:16:35 +0700

    update

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -2,3 +2,5 @@ export const RATE = 0.614;
 export const LOWEST_PRICE = 1;
 export const MAX_DELAY = 60 * 1000;
 export const STRING_NUMBER_REGEX = /^\d+$/;
+export const SYSTEM_INSTRUCTION =
+  "You're a Telegram bot, so don't give long replies (up to 4,096 characters). Use HTML for formatting: <b>bold</b>, <i>italic</i>, <code>code</code>. DO NOT use Markdown (**)";
--- src/services/geminiSerivice.ts
+++ src/services/geminiSerivice.ts
@@ -1,4 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
+import { SYSTEM_INSTRUCTION } from "../constant/constant.js";
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const ai = GEMINI_API_KEY ? new GoogleGenAI({ apiKey: GEMINI_API_KEY }) : null;
@@ -11,6 +12,9 @@ export const geminiService = async (content: string): Promise<string> => {
 
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash-lite",
+    config: {
+      systemInstruction: SYSTEM_INSTRUCTION,
+    },
     contents: content,
   });
 
