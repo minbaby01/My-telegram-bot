797e1978968ffd88ac9d2ad34d193338bf861614
minbaby01 <ongbeo111@gmail.com>
2026-02-19 18:46:38 +0700

    up

--- api/telegram.ts
+++ api/telegram.ts
@@ -1,6 +1,9 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { bot } from "../src/bot.js";
 import { guard } from "../src/guard/guard.js";
+import { webhookCallback } from "grammy";
+
+const handleUpdate = webhookCallback(bot, "std/http");
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
@@ -13,12 +16,13 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const body = req.body;
 
-    await bot.handleUpdate(body);
+    await handleUpdate(body);
 
     return res.status(200).json({
       message: "OK",
     });
   } catch (err) {
+    console.error(err);
     return res.status(200).json({
       message: err,
     });
