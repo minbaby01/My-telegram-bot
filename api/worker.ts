f149d769ba25e94b9ec0894808fe57b41e343e7f
minbaby01 <ongbeo111@gmail.com>
2026-02-20 20:28:05 +0700

    up

--- api/worker.ts
+++ api/worker.ts
@@ -1,12 +1,30 @@
-import { verifySignature } from "@upstash/qstash/nextjs";
 import { cb } from "../src/bot.js";
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Receiver } from "@upstash/qstash";
 
-const worker = async (req: VercelRequest, res: VercelResponse) => {
-  return await cb(req, res);
-};
+const receiver = new Receiver({
+  currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY!,
+  nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY!,
+});
 
-export default verifySignature(worker);
+export default async function worker(req: VercelRequest, res: VercelResponse) {
+  try {
+    const rawBody = req.body;
+
+    const isValid = await receiver.verify({
+      signature: req.headers["upstash-signature"] as string,
+      body: rawBody,
+    });
+
+    if (!isValid) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    return await cb(req, res);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+}
 
 export const config = {
   maxDuration: 60,
